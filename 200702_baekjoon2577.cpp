fcd6267c174685a88c392507a0c78737c05ee3f9
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-03 18:25:21 +0900

    Update and rename baekjoon_2577.cpp to 200702_baekjoon2577.cpp

--- 200702_baekjoon2577.cpp
+++ 200702_baekjoon2577.cpp
@@ -10,12 +10,12 @@ int main(int argc, char* argv[]) {
 	// a*b*c의 최댓값은 4바이트 미만이므로 result는 int형으로 충분!
 	result = a*b*c;
 	
-	while(result) {       //result가 0이 아니면
-		nCnt[result%10]++;  //1의자리수 카운트 +1
-		result /= 10;       //카운트 한 숫자 날려버리기~
+	while(result) {       	  //result가 0이 아니면
+		nCnt[result%10]++;//1의자리수 카운트 +1
+		result /= 10;     //reslut의 일의자리 버리기
 	}
 	
-  //카운트 배열 요소 출력
+ 	 //카운트 배열 요소 출력
 	for(int i=0; i<10; i++) {
 		cout << nCnt[i] << endl;
 	}
