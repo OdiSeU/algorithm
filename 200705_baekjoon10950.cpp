fdb289d2c7eed0854864a348c1d15611ba1dbdd5
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-05 16:33:42 +0900

    Create 200705_baekjoon10950.cpp

---
+++ 200705_baekjoon10950.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int testNum, num1, num2;
+	cin >> testNum;
+	while(testNum--) {
+		cin >> num1 >> num2;
+		cout << num1+num2 << endl;
+	}
+	
+	return 0;
+}
