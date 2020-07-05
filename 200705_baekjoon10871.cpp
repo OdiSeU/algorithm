713127ae197384a128ce0cd7dda542dab6b9d017
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-05 16:31:36 +0900

    Create 200705_baekjoon10871.cpp

---
+++ 200705_baekjoon10871.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int nums, stdNum, input;
+	cin >> nums >> stdNum;
+	while(nums--) {
+		cin >> input;
+		if(input < stdNum) cout << input << " ";
+	}
+	
+	return 0;
+}
