b3ca40594071eaa6dab6bdf93944356f19eaaed6
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-05 16:26:04 +0900

    Create 200705_baekjoon10818.cpp

---
+++ 200705_baekjoon10818.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int nums, input, min=1000000, max=-1000000;
+	cin >> nums;
+	while(nums--) {
+		cin >> input;
+		if(input < min) min = input;
+		if(max < input) max = input;
+	}
+	cout << min << " " << max;
+}
