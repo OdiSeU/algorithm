1886c8b6e84da6160ebcdc711380d52654a55bb8
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-15 21:56:20 +0900

    Create 200715_baekjoon11050.cpp

---
+++ 200715_baekjoon11050.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int binoCoef(int optionNum, int chooseNum) {
+	int num=1, deno=1; // 분자, 분모
+	for(int i=1; i<=optionNum; i++) {
+		if(i<=chooseNum) deno*=i;
+		if(i>optionNum - chooseNum) num*=i;
+	}
+	return num / deno;
+}
+
+int main() {
+	int optionNum, chooseNum;
+	cin >> optionNum >> chooseNum;
+	cout << binoCoef(optionNum, chooseNum) << '\n';
+	return 0;
+}
