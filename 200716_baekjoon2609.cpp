bcfefe0bca43a4e08cf38be1b7c85adffd865fe3
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-16 17:06:25 +0900

    Create 200716_baekjoon2609.cpp

---
+++ 200716_baekjoon2609.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int calcGCD(int a, int b) {
+	while(b>0){
+		int tmp = b;
+		b = a%b;
+		a = tmp;
+	}
+	return a;
+}
+
+int calcLCM(int a, int b) {
+	return a*b/calcGCD(a,b);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << calcGCD(a,b) << '\n' << calcLCM(a,b);
+	return 0;
+}
