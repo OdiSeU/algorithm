b1d9bc2a7ff63e382d1c079679e84b98d27b0ee2
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-17 13:39:34 +0900

    Create 200717_baekjoon2839.cpp

---
+++ 200717_baekjoon2839.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int kg, cnt=0;
+	cin >> kg;
+	while(kg%5){
+		kg -= 3;
+		cnt++;
+	}
+	if(kg<0) cnt = -1;
+	else cnt += kg/5;
+	
+	cout << cnt;
+	
+	return 0;
+}
