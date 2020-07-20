79f249b78b1b152d512f2faf6ba1fc0175ea3e7b
OdiSeU <40794837+OdiSeU@users.noreply.github.com>
2020-07-21 00:26:22 +0900

    Update 200720_baekjoon1012.cpp

--- 200720_baekjoon1012.cpp
+++ 200720_baekjoon1012.cpp
@@ -1,31 +1,51 @@
 #include<iostream>
 using namespace std;
-void checkArea(bool** field, int r, int c) {
 
+int row, col;
+
+void checkArea(bool** field, int r, int c) {
+	field[r][c] = 0;
+	if (r - 1 >= 0 && field[r - 1][c]) {
+		checkArea(field, r - 1, c);
+	}
+	if (c - 1 >= 0 && field[r][c - 1]) {
+		checkArea(field, r, c - 1);
+	}
+	if (r + 1 < row && field[r + 1][c]) {
+		checkArea(field, r + 1, c);
+	}
+	if (c + 1 < col && field[r][c + 1]) {
+		checkArea(field, r, c + 1);
+	}
 }
 
 int main() {
 	int testNum;
 	cin >> testNum;
 	while (testNum--) {
-		int row, col, cbgNum, cnt = 0;
+		int cbgNum, cnt = 0;
 		cin >> row >> col >> cbgNum;
 		bool** field = new bool* [row];
 		int** cbgArr = new int* [cbgNum];
 		for (int i = 0; i < row; i++) {
 			field[i] = new bool[col];
+			for (int j = 0; j < col; j++) {
+				field[i][j] = 0;
+			}
 		}
 		for (int i = 0; i < cbgNum; i++) {
 			cbgArr[i] = new int[2];
 			cin >> cbgArr[i][0] >> cbgArr[i][1];
-			field[cbgArr[i][0]][cbgArr[i][1]] = 0;
+			field[cbgArr[i][0]][cbgArr[i][1]] = 1;
 		}
-
 		for (int i = 0; i < cbgNum; i++) {
-			if (field[cbgArr[i][0]][cbgArr[i][1]] == 0) {
+			if (field[cbgArr[i][0]][cbgArr[i][1]]) {
 				cnt++;
 				checkArea(field, cbgArr[i][0], cbgArr[i][1]);
 			}
 		}
+		cout << cnt << endl;
 	}
+
+	return 0;
 }
